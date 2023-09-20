7aad41ec6ae4b9af28e347dc319728d067cc1de4
riceEnjoyer <144830709+riceEnjoyer@users.noreply.github.com>
2023-09-20 13:06:38 -0400

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,26 @@
+public class Methods {
+    public static void main(String [] args){
+        method1(5);
+        method1(92);
+        int r1 = method2("Arjun", 7);
+        System.out.println(r1);
+        int r2 = method2("Serena", 8);
+        System.out.println(r2);
+        System.out.println(method3(4, 5.5));
+    }
+
+    public static double method3(int y, double z){
+        double answer = y+6*z-2;
+        return answer; 
+
+    }
+    public static int method2(String name, int x){
+        System.out.println("Hello" +name);
+        int rem = x%2;
+        return rem;
+    }
+    public static void method1(int x){
+        int y = x + 1;
+        System.out.println("One more than "+ y);
+    }
+}
